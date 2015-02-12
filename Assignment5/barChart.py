39c6a9ee992e6b89c849f3b09eb3574bc2a93269
Daniel Ni <danielmichaelni@gmail.com>
2015-02-11 22:45:25 -0600

    finish part 4

--- Assignment5/barChart.py
+++ Assignment5/barChart.py
@@ -2,12 +2,15 @@ import unicodecsv as csv
 import matplotlib.pyplot as plt
 
 def getBarChartData():
+    # open necessary files
     f_artists = open('artists.csv')
     f_albums = open('albums.csv')
 
+    # create readers for each file
     artists_rows = csv.reader(f_artists)
     albums_rows = csv.reader(f_albums)
 
+    # read header line from csv files
     artists_header = artists_rows.next()
     albums_header = albums_rows.next()
 
@@ -19,14 +22,20 @@ def getBarChartData():
         decade_dict[decade] = 0
     
     for artist_row in artists_rows:
+        # skip the row if it is blank
         if not artist_row:
             continue
+        
+        # extract info from row and add artist name to artist_names list
         artist_id,name,followers, popularity = artist_row
         artist_names.append(name)
 
     for album_row  in albums_rows:
+        # skip the row if it is blank
         if not album_row:
             continue
+
+        # extract info from row and increment decades_dict accordingly
         artist_id, album_id, album_name, year, popularity = album_row
         for decade in decades:
             if (int(year) >= int(decade)) and (int(year) < (int(decade) + 10)):
@@ -40,8 +49,11 @@ def getBarChartData():
 def plotBarChart():
     x_vals, y_vals, artist_names = getBarChartData()
     
+    # plot a bar graph of number of albums per decade
     fig , ax = plt.subplots(1,1)
     ax.bar(x_vals, y_vals, width=10)
+
+    # set labels
     ax.set_xlabel('decades')
     ax.set_ylabel('number of albums')
     ax.set_title('Totals for ' + ', '.join(artist_names))

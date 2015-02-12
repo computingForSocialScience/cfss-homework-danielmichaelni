0bd3e77aeb5f03c360cdfc8d5b81a1b47af32058
Daniel Ni <danielmichaelni@gmail.com>
2015-02-11 21:04:32 -0600

    finish part 1

--- Assignment5/fetchArtist.py
+++ Assignment5/fetchArtist.py
@@ -2,16 +2,23 @@ import sys
 import requests
 import csv
 
+SPOTIFY_API_BASE_URL = 'https://api.spotify.com'
+
 def fetchArtistId(name):
     """Using the Spotify API search method, take a string that is the artist's name, 
     and return a Spotify artist ID.
     """
-    pass
+    url = SPOTIFY_API_BASE_URL + '/v1/search/?q=' + name + '&type=artist'
+    req = requests.get(url)
+    data = req.json()
+    return data['artists']['items'][0]['id']
 
 def fetchArtistInfo(artist_id):
     """Using the Spotify API, takes a string representing the id and
 `   returns a dictionary including the keys 'followers', 'genres', 
     'id', 'name', and 'popularity'.
     """
-    pass
-
+    url = SPOTIFY_API_BASE_URL + '/v1/artists/' + id
+    req = requests.get(url)
+    data = req.json()
+    return data

607eaa5106c00ad0fd4c7c3dec2bc4af466f3a0e
Daniel Ni <danielmichaelni@gmail.com>
2015-02-11 21:25:28 -0600

    finish part 2

--- Assignment5/fetchAlbums.py
+++ Assignment5/fetchAlbums.py
@@ -1,15 +1,25 @@
 import requests
 from datetime import datetime
 
+SPOTIFY_API_BASE_URL = 'https://api.spotify.com'
+
 def fetchAlbumIds(artist_id):
     """Using the Spotify API, take an artist ID and 
     returns a list of album IDs in a list
     """
-    pass
+    url = SPOTIFY_API_BASE_URL + '/v1/artists/' + id + '/albums?market=US&album_type=album'
+    req = requests.get(url)
+    data = req.json()
+    albumIds = []
+    for album in data['items']:
+        albumIds.append(album['id'])
+    return albumIds
 
 def fetchAlbumInfo(album_id):
     """Using the Spotify API, take an album ID 
     and return a dictionary with keys 'artist_id', 'album_id' 'name', 'year', popularity'
     """
-    pass
-
+    url = SPOTIFY_API_BASE_URL + '/v1/albums/' + id
+    req = requests.get(url)
+    data = req.json()
+    return data

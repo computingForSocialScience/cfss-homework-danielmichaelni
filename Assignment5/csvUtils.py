5df748cc9bfb29af4f20625e7d4e26f5bb987a2b
Daniel Ni <danielmichaelni@gmail.com>
2015-02-11 23:17:25 -0600

    fixed small errors

--- Assignment5/csvUtils.py
+++ Assignment5/csvUtils.py
@@ -15,7 +15,7 @@ def writeArtistsTable(artist_info_list):
         artist_followers = unicode(artist_info['followers'])
         artist_popularity = unicode(artist_info['popularity'])
         f.write(artist_id + u',"' + artist_name + u'",' + artist_followers + u',' + artist_popularity + u'\n')
-    f.close('artsits.csv')
+    f.close()
       
 def writeAlbumsTable(album_info_list):
     """
@@ -27,12 +27,12 @@ def writeAlbumsTable(album_info_list):
     ARTIST_ID,ALBUM_ID,ALBUM_NAME,ALBUM_YEAR,ALBUM_POPULARITY
     """
     f = open('albums.csv', 'w')
-    f.write(u'ARTIST_ID,ALBUM_ID,ALBUM_NAME,ALBUM_YEAR,ALBUM_POPULARITY')
+    f.write(u'ARTIST_ID,ALBUM_ID,ALBUM_NAME,ALBUM_YEAR,ALBUM_POPULARITY\n')
     for album_info in album_info_list:
         artist_id = album_info['artist_id']
         album_id = album_info['album_id']
         album_name = album_info['name']
         album_year = album_info['year']
         album_popularity = unicode(album_info['popularity'])
-        f.write(artist_id + u',' + album_id + u',' + album_name + u',' + album_year + u',' + album_popularity)
+        f.write(artist_id + u',' + album_id + u',"' + album_name + u'",' + album_year + u',' + album_popularity + u'\n')
     f.close()

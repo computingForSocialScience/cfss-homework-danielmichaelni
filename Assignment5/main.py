e66dfd5433a5adfdb11b4f8087e2226c9532c740
Daniel Ni <danielmichaelni@gmail.com>
2015-02-11 23:18:04 -0600

    finish part 5

--- Assignment5/main.py
+++ Assignment5/main.py
@@ -8,5 +8,17 @@ if __name__ == '__main__':
     artist_names = sys.argv[1:]
     print "input artists are ", artist_names
     # YOUR CODE HERE
-    
+    artist_info_list = []
+    album_info_list = []
+    for name in artist_names:
+        artist_id = fetchArtistId(name)
+        artist_info = fetchArtistInfo(artist_id)
+        artist_info_list.append(artist_info)
 
+        album_ids = fetchAlbumIds(artist_id)
+        for id in album_ids:
+            album_info = fetchAlbumInfo(id)
+            album_info_list.append(album_info)
+    writeArtistsTable(artist_info_list)
+    writeAlbumsTable(album_info_list)
+    plotBarChart()
